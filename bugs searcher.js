26341a8cc3f403b935f085ee56e96edd1e5d092e
Alex Malevsky 2 <154023677+Alex23Malevsky@users.noreply.github.com>
2023-12-16 23:20:37 +0200

    Create bugs searcher.js

---
+++ bugs searcher.js
@@ -0,0 +1,21 @@
+const settings =
+  typeof process.env.NEXT_PUBLIC_SETTINGS === 'string'
+    ? JSON.parse(process.env.NEXT_PUBLIC_SETTINGS)
+    : {}
+
+ publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    settings
+  }
+
+{
+  "compilerOptions": {
+    "target": "es5",
+    "lib": ["dom", "dom.iterable", "esnext"],
+    "allowJs": true,
+    "skipLibCheck": true,
+    "strict": true,
+    "forceConsistentCasingInFileNames": true,
+    "noEmit": true,}
+}
